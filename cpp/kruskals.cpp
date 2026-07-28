664f010f10b21b8e2e29c1abb26b7b090e90dffd
Kobe Bayliss <kobebayliss1@gmail.com>
2026-07-28 19:34:47 +1200

    feat: adding union find to kruskal's

--- cpp/kruskals.cpp
+++ cpp/kruskals.cpp
@@ -2,12 +2,19 @@
 #include <vector>
 #include <memory>
 #include <queue>
-#include <unordered_set>
 #include <tuple>
 #include <functional>
 
 using AdjMatrix = std::vector<std::vector<size_t>>;
 using EdgeData = std::tuple<size_t, size_t, size_t>;  // edge weight, node 1, node 2
+
+size_t fetch_root(std::vector<size_t> &parent, size_t x) {
+	while (parent[x] != x) {
+		parent[x] = parent[parent[x]];
+		x = parent[x];
+	}
+	return x;
+}
 
 int main() {
 	size_t nodes;
@@ -28,9 +35,10 @@ int main() {
 	for (size_t i = 0; i < graphs.size(); i++) {
 		size_t total_weight = 0;
 		AdjMatrix& graph = *graphs[i];
-		std::unordered_set<size_t> seen;
+		std::vector<size_t> parent(graph.size());
 		std::priority_queue<EdgeData, std::vector<EdgeData>, std::greater<EdgeData>> edges;
 		for (size_t j = 0; j < graph.size(); j++) {
+			parent[j] = j;
 			for (size_t k = j + 1; k < graph[j].size(); k++) {
 				size_t edge_weight = graph[j][k];
 				if (edge_weight != 0) {
@@ -44,11 +52,12 @@ int main() {
 			edges.pop();
 			size_t edge1 = std::get<1>(edge);
 			size_t edge2 = std::get<2>(edge);
-			if (seen.find(edge1) != seen.end() && seen.find(edge2) != seen.end()) {
+			size_t root1 = fetch_root(parent, edge1);
+			size_t root2 = fetch_root(parent, edge2);
+			if (root1 == root2) {
 				continue;
 			}
-			seen.insert(edge1);
-			seen.insert(edge2);
+			parent[root1] = root2;
 			total_weight += std::get<0>(edge);
 		}
 		std::cout << total_weight << std::endl;
